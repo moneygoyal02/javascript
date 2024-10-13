19e4a5d4f790c672d1f06dbceda114424e456cb4
moneygoyal02 <goyalmoney95@gmail.com>
2024-10-13 19:53:32 +0530

    basics

---
+++ Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// { }  this curly braces is called as block scope
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity , accountState])
