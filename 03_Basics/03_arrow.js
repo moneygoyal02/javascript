126942e7ceb86088c441b0a28cc0832370bbd7e4
moneygoyal02 <goyalmoney95@gmail.com>
2024-10-16 23:55:22 +0530

    arrows done

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -4,7 +4,7 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(this);  // here this will refer to user object and output will be {username: "hitesh", price: 999, welcomeMessage: ƒ}
     }
 
 }
@@ -13,40 +13,40 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);
+// console.log(this); // {}
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); 
 // }
 
 // chai()
 
 // const chai = function () {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); // undefined
 // }
 
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this); // undefined
 }
 
 
-// chai()
+chai()
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  num1 + num2 // implicit return no need to write return keyword
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// const addTwo = (num1, num2) => ( num1 + num2 ) // implicit return no need to write return keyword
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "hitesh"}) 
 
 
-console.log(addTwo(3, 4))
+console.log(addTwo(3, 4))  // undefined if  {username: "hitesh"} this is not in () because it will consider {} as block of code
 
 
 // const myArray = [2, 5, 3, 7, 8]
